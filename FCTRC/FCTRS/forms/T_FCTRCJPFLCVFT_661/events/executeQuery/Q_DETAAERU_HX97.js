ee9f573dc9d521f1ff96c5e9fd6b040a03c4e11f
Andreess237 <andresrue222@hotmail.com>
2021-04-23 17:08:35 -0500

    Agrego algunos formularios de facturacion y entidad detalleFactura

---
+++ FCTRC/FCTRS/forms/T_FCTRCJPFLCVFT_661/events/executeQuery/Q_DETAAERU_HX97.js
@@ -0,0 +1,6 @@
+
+//DetalleFacturacionGrupoAQuery Entity: EntidadDetalleFactura
+task.executeQuery.Q_DETAAERU_HX97 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.EntidadDetalleFactura = true;
+};
