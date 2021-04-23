042b8df2c601aa97eb4a147d1eb94f21b1b97651
Milton Paucar 999 <miltonpaucar999@gmail.com>
2021-04-23 12:54:25 -0500

    unidos formularios, entidades y querys de producto y cliente, funcionando

---
+++ FCTRC/FCTRS/forms/T_FCTRCOHOJWGYR_975/events/executeQuery/Q_PRODCUPP_OW53.js
@@ -0,0 +1,6 @@
+
+//ProductoGrupoAQuery Entity: ProductoGrupoA
+task.executeQuery.Q_PRODCUPP_OW53 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.ProductoGrupoA = true;
+};
