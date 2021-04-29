d6509d8b42682d0d80375f6ab6ea27338e83acae
kary17 <vero_kary1995@hotmail.com>
2021-04-29 12:39:53 -0500

    buscar producto por codigo

---
+++ FCTRC/FCTRS/forms/T_FCTRCTCUUEVUO_237/events/change/VA_2PIYOVSTVVKERFH_216702.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EntidadDetalleFactura
+//EntidadDetalleFactura.codigoProducto (DropDownList) View: FormularioBuscarProdGrupoA
+//Evento Change: Se ejecuta al cambiar el valor de un InputControl.
+task.change.VA_2PIYOVSTVVKERFH_216702 = function(  entities, changedEventArgs ) {
+
+    changedEventArgs.commons.execServer = false;
+    changedEventArgs.newValue;
+
+};
